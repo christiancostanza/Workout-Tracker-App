1c14f5d1b503ae9243dda139d797abf7ca8489f7
christiancostanza <christiancostanza2003@gmail.com>
2025-12-11 15:58:34 -0600

    fixed typo

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ import workoutRoutes from './routes/workouts.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to DB
 connectDB();
