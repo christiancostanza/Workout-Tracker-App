33e1552d5b4b877c66ee64958d854f3613344445
christiancostanza <christiancostanza2003@gmail.com>
2025-12-14 13:39:15 -0600

    working back to netlify

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Workout-Tracker-App/' // replace with your actual repo name
+  base: '/' 
 });
